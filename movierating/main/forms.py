9f76036109ceb955ae2abd1ea97c1010d8ea71a7
yonasyiheyis <49254355+yonasyiheyis@users.noreply.github.com>
2020-06-03 21:42:39 +0300

    Add files via upload

---
+++ movierating/main/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import *
+
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = ('name', 'director', 'cast', 'description', 'release_date', 'image')
